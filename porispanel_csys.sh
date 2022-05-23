bd1de374b4f38b2490dd3035ab61d101c31e33a8
Txinto Vaz <txinto.vaz@gtc.iac.es>
2022-05-23 20:37:47 +0100

    Actualizamos pyPORIS y scripts para que sean más seguros

--- porispanel_csys.sh
+++ porispanel_csys.sh
@@ -1,10 +1,50 @@
 #!/bin/bash
-rm models/$1.ods
-rm models/$1.xml
-cp pyPORIS/config_rm_enabled.py pyPORIS/config_rm.py
-python3 pyPORIS/graph2poris.py models/$1.graphml || { echo "graph2poris could not be processed"; exit 1; } 
-mv models/$1.graphml models/$1.old.graphml
-mv models/$1.out.graphml models/$1.graphml
-python3 pyPORIS/poris2xml.py models/$1.ods || { echo "poris2xml could not be processed"; exit 1; } 
-java -jar pyPORIS/AstroPorisPlayer/bin/AstroPorisPlayer.jar models/$1.xml
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1;
+fi
+
+FILE=models/$1.graphml
+FILE1=models/$1.ods
+FILE2=models/$1.out.graphml
+FILE3=models/$1.xml
+
+if test -f "$FILE"; then
+    echo "Input $FILE exists, continuing"
+else
+    echo "Input $FILE does not exist, aborting"
+    exit 1;
+fi
+
+rm $FILE1
+rm $FILE3
+
+cp pyPORIS/config_rm_enabled.py pyPORIS/config_rm.py || { echo "config_rm_enabled.py missing"; exit 1; } 
+python3 pyPORIS/graph2poris.py $FILE || { echo "graph2poris could not be processed"; exit 1; }
+if test -f "$FILE1"; then
+    echo "Input $FILE1 exists, continuing"
+else
+    echo "Input $FILE1 does not exist, aborting"
+    exit 1;
+fi
+
+if test -f "$FILE2"; then
+    echo "Input $FILE2 exists, continuing"
+else
+    echo "Input $FILE2 does not exist, aborting"
+    exit 1;
+fi
+timestamp=$(date +%s)
+cp $FILE models/$1.$timestamp.backup
+mv $FILE models/$1.old.graphml
+mv $FILE2 $FILE
+python3 pyPORIS/poris2xml.py $FILE1 || { echo "poris2xml could not be processed"; exit 1; } 
+if test -f "$FILE3"; then
+    echo "Input $FILE3 exists, continuing"
+else
+    echo "Input $FILE3 does not exist, aborting"
+    exit 1;
+fi
+java -jar pyPORIS/AstroPorisPlayer/bin/AstroPorisPlayer.jar $FILE3
 
